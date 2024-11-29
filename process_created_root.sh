55d6033b3109f11110362152b767b2d55965fffe
Admin <admin@Admins-MacBook-Pro-8.local>
2024-11-29 10:38:32 +0530

    second commit

--- process_created_root.sh
+++ process_created_root.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ps -U root -u root -f
 # this is created by lakshmi
+#created on 26th nov 2024
 #ps : stands for status of process
 #-U root : gives list of process created by root(by using real user ID)
 #-u root :another way to specipy process created by rooy user(by using their effective user ID)
